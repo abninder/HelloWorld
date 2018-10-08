32738ae6844ff151db6ee670119f8002949c9415
Abninder Gill <abninder@Abninders-MBP.lan>
2018-10-08 20:59:25 +0100

    initial checkin

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+    //A tree map does a replace for duplicate keys, it does not form a chain like a hash map
+    public static void main (String [] args)
+    {
+        for(int i=0; i<10; i++)
+            System.out.println("hello world");
+    }
+
+}
